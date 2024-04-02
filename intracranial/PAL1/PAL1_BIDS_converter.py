a8fc89a5f312e18e741d35859314836f7c21ea91
hherrema <hherrema@sas.upenn.edu>
2024-04-02 14:27:50 -0400

    with math events
    revert changes used to write single session

--- intracranial/PAL1/PAL1_BIDS_converter.py
+++ intracranial/PAL1/PAL1_BIDS_converter.py
@@ -33,20 +33,17 @@ class PAL1_BIDS_converter(intracranial_BIDS_converter):
         events['duration'] = np.concatenate((np.diff(events.mstime), np.array([0]))) / 1000.0   # event duration [s] --> lots of superfluous events may mess this up
         events['duration'] = events['duration'].mask(events['duration'] < 0.0, 0.0)             # replace events with negative duration with 0.0s
         events['response_time'] = 'n/a'                                                         # response time [s]
-        #events.loc[events.trial_type=='PROB', 'response_time'] = events['rectime']              # math events use rectime
+        events.loc[events.trial_type=='PROB', 'response_time'] = events['rectime']              # math events use rectime
         events.loc[events.trial_type=='REC_EVENT', 'response_time'] = events['RT']              # recall events use RT
         # stim_file, serialpos, probepos, 
         
         events = events.fillna('n/a')                    # change NaN to 'n/a'
         events = events.replace('', 'n/a')               # no empty cells
-        """
+
         events = events[['onset', 'duration', 'sample', 'trial_type', 'response_time', 
                          'serialpos', 'probepos', 'probe_word', 'resp_word', 'study_1', 'study_2', 
                          'list', 'test', 'answer', 'experiment', 'session', 'subject']]         # re-order columns
-        """
-        events = events[['onset', 'duration', 'sample', 'trial_type', 'response_time', 
-                         'serialpos', 'probepos', 'probe_word', 'resp_word', 'study_1', 'study_2', 
-                         'list', 'experiment', 'session', 'subject']]         # re-order columns
+
         return events
     
     def make_events_descriptor(self):
