7dfebc1333b90ca8cd911221559a9566e5093b21
hherrema <hherrema@sas.upenn.edu>
2024-09-23 15:18:19 -0400

    change block_num to trial

--- intracranial/YC1/YC1_BIDS_converter.py
+++ intracranial/YC1/YC1_BIDS_converter.py
@@ -31,7 +31,7 @@ class YC1_BIDS_converter(intracranial_BIDS_converter):
         events = self.reader.load('events')
         events = self.expand_travel_paths(events)                     # expand events to have rows for travel paths
 
-        events = events.rename(columns={'eegoffset': 'sample', 'type': 'trial_type', 'resp_travel_time': 'resp_total_time'})     # rename columns
+        events = events.rename(columns={'eegoffset': 'sample', 'type': 'trial_type', 'block_num': 'trial', 'resp_travel_time': 'resp_total_time'})     # rename columns
         events['onset'] = (events.mstime - events.mstime.iloc[0]) / 1000.0                      # onset from first event [s]
         events['duration'] = np.concatenate((np.diff(events.mstime), np.array([0]))) / 1000.0   # event duration [s] --> lots of superfluous events may mess this up
         events['duration'] = events['duration'].mask(events['duration'] < 0.0, 0.0)             # replace events with negative duration with 0.0s
